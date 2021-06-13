532e89a738fa427ef83834092300e3d7d99fae15
Thales Lima <M6104902@mgmt.tlrg.com>
2021-06-13 11:29:31 -0300

    Implementing controllers

--- DevFitness.API/Controllers/MealsController.cs
+++ DevFitness.API/Controllers/MealsController.cs
@@ -1,43 +1,97 @@
-﻿using DevFitness.API.Models.InputModels;
+﻿using DevFitness.API.Core.Entities;
+using DevFitness.API.Models.InputModels;
+using DevFitness.API.Models.ViewModels;
+using DevFitness.API.Repositories.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace DevFitness.API.Controllers
 {
     [Route("api/users/{userId}/meals")]
     public class MealsController : ControllerBase
     {
+        private readonly IMealRepository _mealRepository;
+
+        public MealsController(IMealRepository mealRepository)
+        {
+            _mealRepository = mealRepository;
+        }
+
         // api/user/4/meals HTTP GET
         [HttpGet]
         public IActionResult GetAll(int userId)
         {
-            return Ok();
+            var allMeals = _mealRepository.GetAll(userId);
+
+            if(!allMeals.Any())
+            {
+                return NotFound("NENHUM ALIMENTO ENCONTRADO!");
+            }
+
+            var allMealsViewModels = allMeals
+                .Select(m => new MealViewModel(m.Id, m.Description, m.Calories, m.Date));
+
+            return Ok(allMealsViewModels);
         }
 
         // api/user/4/meals/16 HTTP GET
         [HttpGet("{mealId}")]
-        public IActionResult Get(int userId, int mealId)
+        public IActionResult Get(int mealId, int userId)
         {
-            return Ok();
+            var meal = _mealRepository.Get(mealId, userId);
+
+            if (meal == null)
+                return NotFound("NENHUM ALIMENTO ENCONTRADO!");
+
+            var mealViewModel = new MealViewModel(meal.Id, meal.Description, meal.Calories, meal.Date);
+
+            return Ok(mealViewModel);
         }
 
         // api/user/4/meals HTTP POST
         [HttpPost]
-        public IActionResult Post(int userId, [FromBody]CreateMealInputModel inputModel)
+        public IActionResult Post(int userId, [FromBody] CreateMealInputModel inputModel)
         {
-            return Ok();
+            var meal = new Meal(inputModel.Description, inputModel.Calories, inputModel.Date, userId);
+
+            _mealRepository.Create(meal);
+
+            return CreatedAtAction(nameof(Get), new
+            {
+                userId = userId,
+                mealId = meal.Id,
+            }, inputModel);
         }
 
         // api/user/4/meals/16 HTTP PUT
         [HttpPut("{mealId}")]
-        public IActionResult Put(int userId, int mealId,  [FromBody]UpdateMealInputModel inputModel)
+        public IActionResult Put(int mealId, int userId, [FromBody] UpdateMealInputModel inputModel)
         {
+            var meal = _mealRepository.Get(mealId, userId);
+
+            if(meal == null)
+            {
+                return NotFound("NENHUM ALIMENTO ENCONTRADO!");
+            }
+
+            _mealRepository.Update(meal, inputModel);
+
             return NoContent();
         }
 
         // api/user/4/meals/16 HTTP DELETE
         [HttpDelete("{mealId}")]
-        public IActionResult Delete(int userId, int mealId)
+        public IActionResult Delete(int mealId, int userId)
         {
+            var meal = _mealRepository.Get(mealId, userId);
+
+            if (meal == null)
+            {
+                return NotFound("NENHUM ALIMENTO ENCONTRADO!");
+            }
+
+            _mealRepository.Delete(meal);
+
             return NoContent();
         }
     }
--- DevFitness.API/Controllers/UsersController.cs
+++ DevFitness.API/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using DevFitness.API.Models.InputModels;
+﻿using DevFitness.API.Core.Entities;
+using DevFitness.API.Models.InputModels;
+using DevFitness.API.Models.ViewModels;
+using DevFitness.API.Repositories.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevFitness.API.Controllers
@@ -6,25 +9,54 @@ namespace DevFitness.API.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
+        private IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            return Ok();
+            var user = _userRepository.Get(id);
+
+            if (user == null)
+            {
+                return NotFound("NENHUM USUÁRIO ENCONTRADO!");
+            }
+
+            var userViewModel = new UserViewModel(user.Id, user.FullName, user.Height, user.Weight, user.BirthDate);
+
+            return Ok(userViewModel);
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody]CreateUserInputModel inputModel)
+        public IActionResult Post([FromBody] CreateUserInputModel inputModel)
         {
-            // return BadRequest
+            var user = new User(inputModel.FullName, inputModel.Height, inputModel.Weight, inputModel.BirthDate);
 
-            return CreatedAtAction(nameof(Get), new { id = 10 }, inputModel);
+            _userRepository.Create(user);
+
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, inputModel);
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody]UpdateUserInputModel inputModel)
+        public IActionResult Put(int id, [FromBody] UpdateUserInputModel inputModel)
         {
-            //return NotFound();
-            //return BadRequest();
+            var user = _userRepository.Get(id);
+
+            if (user == null)
+            {
+                return NotFound("NENHUM USUÁRIO ENCONTRADO!");
+            }
+
+            if(inputModel.Height < user.Height)
+            {
+                return BadRequest("NÃO É POSSÍVEL INSERIR UMA ALTURA MENOR DO QUE O ATUAL");
+            }
+
+            _userRepository.Update(user, inputModel);
 
             return NoContent();
         }
